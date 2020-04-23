db81cda9b0f23a0c31abffeeaf636f362d153927
rizalord <ahmadkhamdani9@gmail.com>
2020-04-23 17:12:15 +0700

    scraping homepage , blogpage

--- controller/MainController.js
+++ controller/MainController.js
@@ -54,30 +54,85 @@ class MainController {
     );
   };
 
-  about = (req, res) => {
+  blog = (req, res) => {
     var page =
       typeof req.params.page == "undefined"
         ? ""
         : req.params.page == "1"
         ? ""
         : `page/${req.params.page.toString()}/`;
-    scraperjs.StaticScraper.create(`https://samehadaku.vip/blog/${page}`).scrape(
-      function ($) {
-        var blog = {};git
-        blog.blog = $(".box-blog")
-          .map(function () {
-            return {
-              title: $(this).find("h2 a").text(),
-              sub: $(this).find(".exp p").text(),
-              date: $(this).find(".auth i").text(),
-              link: $(this).find(".img a").attr("href"),
-            };
-          })
-          .get();
+    scraperjs.StaticScraper.create(
+      `https://samehadaku.vip/blog/${page}`
+    ).scrape(function ($) {
+      var blog = {};
+      blog.blog = $(".box-blog")
+        .map(function () {
+          return {
+            title: $(this).find("h2 a").text(),
+            sub: $(this).find(".exp p").text(),
+            date: $(this).find(".auth i").text(),
+            link: $(this).find(".img a").attr("href"),
+            linkId: $(this)
+              .find(".img a")
+              .attr("href")
+              .replace("https://samehadaku.vip/blog/", "")
+              .replace("/", "")
+              .trim(),
+            image: $(this).find(".img a img").attr("src"),
+          };
+        })
+        .get();
 
-        res.send(blog);
-      }
-    );
+      res.send(blog);
+    });
+  };
+
+  readblog = (req, res) => {
+    const id = req.params.id;
+    const page = `https://samehadaku.vip/blog/${id}`;
+
+    // REQUEST TO SAMEHADAKU
+    scraperjs.StaticScraper.create(page).scrape(function ($) {
+      var data = {};
+      data.title = $(".sttle h1.entry-title")
+        .map(function () {
+          return $(this).text();
+        })
+        .get()[0];
+      data.author = $(".author.vcard")
+        .map(function () {
+          return $(this).find("span").text();
+        })
+        .get()[0];
+      data.date_created = $("span.date")
+        .map(function () {
+          return $(this).text();
+        })
+        .get()[0];
+      data.image_cover = $(".thumb-blog").map(function(){ return $(this).find('img').attr('src') }).get()[0];
+      data.content = $(".entry-content.content-post p")
+        .filter(function (e, i) {
+          if ($(this).text() == "") {
+            data.image_content = $(this).find("img").attr("src");
+          }
+          return $(this).text() != "";
+        })
+        .map(function () {
+          return $(this).text();
+        })
+        .get();
+      data.tags = $(".post_taxs a")
+        .map(function (params) {
+          return {
+            title: $(this).text(),
+            link: $(this).attr("href"),
+            active: $(this).attr("class") == "posts_tags" ? false : true,
+          };
+        })
+        .get();
+
+      res.send(data);
+    });
   };
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const MainController = require('./../controller/MainController');
-var express = require('express');
+const MainController = require("./../controller/MainController");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", MainController.home);
-router.get('/page/:page', MainController.home);
-router.get("/about/", MainController.about);
-router.get("/about/:page", MainController.about);
+router.get("/page/:page", MainController.home);
+router.get("/blog/", MainController.blog);
+router.get("/blog/read/:id", MainController.readblog);
+router.get("/blog/:page", MainController.blog);
 
 module.exports = router;
